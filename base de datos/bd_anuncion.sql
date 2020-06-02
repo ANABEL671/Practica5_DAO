5806c9e319eebe582c0811dedb924763e8832282
ANABEL671 <48634106+ANABEL671@users.noreply.github.com>
2020-06-02 17:28:33 -0400

    Add files via upload

---
+++ base de datos/bd_anuncion.sql
@@ -0,0 +1,73 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 02-06-2020 a las 23:27:16
+-- Versión del servidor: 10.4.11-MariaDB
+-- Versión de PHP: 7.3.18
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `bd_anuncion`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `avisos`
+--
+
+CREATE TABLE `avisos` (
+  `id` int(20) NOT NULL,
+  `titulo` varchar(100) DEFAULT NULL,
+  `contenido` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `avisos`
+--
+
+INSERT INTO `avisos` (`id`, `titulo`, `contenido`) VALUES
+(1, 'de ocacion', 'en venta una laptop core i7 con 8 de ram'),
+(2, 'En venta', 'lote de libros de computacion'),
+(4, 'de acacion', 'en venta una casa'),
+(5, 'en venta', 'una computadora nueva'),
+(6, 'en venta', ' refrigeradora marca samsung'),
+(7, 'en venta', 'una computadora con ram de 4'),
+(8, 'de ocacion ', 'en venta un vehiculo modelo 2018'),
+(9, 'de ocacion', 'en venta una laptop core i7 con 8 de ram yaudifunos de regalo');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `avisos`
+--
+ALTER TABLE `avisos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `avisos`
+--
+ALTER TABLE `avisos`
+  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
